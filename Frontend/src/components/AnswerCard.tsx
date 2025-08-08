eeda8948ce31ebff0239c59db11cd6d41f5f67d2
ab1ndal <bindal.abhinav@gmail.com>
2025-08-08 13:50:34 -0700

    Typo

--- Frontend/src/components/AnswerCard.tsx
+++ Frontend/src/components/AnswerCard.tsx
@@ -1,6 +1,4 @@
 // src/components/AnswerCard.tsx
-// src/components/AnswerCard.tsx
-import React from "react";
 import ReactMarkdown from "react-markdown";
 import rehypeHighlight from "rehype-highlight";
 import { Badge } from "@/components/ui/badge";
